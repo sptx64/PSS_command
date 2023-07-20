2ffbb835e45c93b5323af7c093eb1d84cd3ebefb
sptx64 <89103497+sptx64@users.noreply.github.com>
2023-07-20 17:11:27 +1100

    Update Tax.py

--- Tax.py
+++ Tax.py
@@ -5,7 +5,6 @@ from streamlit_extras.metric_cards import style_metric_cards
 
 
 "# Greenfest Ouenghi"
-"Application web pour éviter de créer un excel à chaque commande PSS."
 
 "### 🏄‍ COMMANDE"
 "Commencez par sélectionner les participants à la commande et le nombre d'articles!"
